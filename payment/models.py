9f1c72eb0b303cca3ec7561af5da7950e109b5eb
anuragkumar <akanuragkumar712@gmail.com>
2024-09-24 00:38:28 +0530

    added Custom validation for the Payment model

--- payment/models.py
+++ payment/models.py
@@ -1,5 +1,6 @@
 import uuid
 
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.transaction import atomic
 from django_fsm import transition
@@ -19,6 +20,24 @@ class Payment(models.Model):
     status = models.CharField(max_length=50, choices=PaymentStatusChoices,
                                        default=PaymentStatusChoices.INITIATED)
     payment_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Payment'
+        verbose_name_plural = 'Payments'
+        indexes = [
+            models.Index(fields=['transaction_id']),
+            models.Index(fields=['status']),
+        ]
+
+    def __str__(self):
+        return f'Transaction {self.transaction_id} - {self.status} - {self.customer.name} - {self.amount}'
+
+    def clean(self):
+        """Custom validation for the Payment model."""
+        if self.amount <= 0:
+            raise ValidationError({'amount': 'Amount must be greater than zero.'})
+        super().clean()
+
 
     @atomic
     @fsm_log_by
@@ -76,9 +95,6 @@ class Payment(models.Model):
     def mark_status_refund_initiated_to_refunded(self, by=None):
         pass
 
-    def __str__(self):
-        """Represent PaymentTransactions object as string."""
-        return '{} {} {}'.format(self.amount, self.status, self.customer.name)
 
 class Cashfree(models.Model):
     payment = models.ForeignKey(Payment, on_delete=models.CASCADE, related_name='cashfree')
