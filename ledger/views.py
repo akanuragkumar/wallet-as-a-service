3f163f5c98e8bcd2526c4eee53366dc93b01dd96
anuragkumar <akanuragkumar712@gmail.com>
2024-09-24 00:46:15 +0530

    added pagination for ledger view GET

--- ledger/views.py
+++ ledger/views.py
@@ -1,9 +1,9 @@
-from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.exceptions import NotFound
+from rest_framework.generics import ListAPIView
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
-from rest_framework.status import (
-HTTP_404_NOT_FOUND,
-HTTP_200_OK
-)
+
 
 from base.authentication.tenant_authentication import APIKeySecretAuthentication
 from base.middleware.CustomMetaDataMixin import CustomMetaDataMixin
@@ -14,16 +14,33 @@ from ledger.models import Ledger
 from ledger.serializer import LedgerResponseSerializer
 
 
-class LedgerView(CustomMetaDataMixin, APIView):
-    authentication_classes = [APIKeySecretAuthentication]
+# Custom pagination class
+class LedgerPagination(PageNumberPagination):
+    page_size = 10  # Number of ledger entries per page
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
-    # GET: Get ledger history for a customer
-    def get(self, request, customer_uuid):
+
+class LedgerView(CustomMetaDataMixin, ListAPIView):
+    authentication_classes = [APIKeySecretAuthentication]
+    serializer_class = LedgerResponseSerializer
+    pagination_class = LedgerPagination
+
+    def get_queryset(self):
+        customer_uuid = self.kwargs['customer_uuid']
         try:
             customer = Customer.objects.get(uuid=customer_uuid)
         except Customer.DoesNotExist:
-            return Response({'error': 'Customer not found'}, status=HTTP_404_NOT_FOUND)
+            raise NotFound(detail="Customer not found")
+        return Ledger.objects.filter(customer=customer).order_by('-created')
 
-        ledger_entries = Ledger.objects.filter(customer=customer).order_by('-created')
-        response_serializer = LedgerResponseSerializer(ledger_entries, many=True)
-        return Response(response_serializer.data, status=HTTP_200_OK)
+    # GET: Get ledger history for a customer with pagination
+    def list(self, request, customer_uuid):
+        queryset = self.get_queryset()
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            response_serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(response_serializer.data)
+
+        response_serializer = self.get_serializer(queryset, many=True)
+        return Response(response_serializer.data, status=status.HTTP_200_OK)
